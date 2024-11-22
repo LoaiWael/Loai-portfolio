59ee5174a44d4c2bdad70d5d558ad9d4503df1ee
Loai Wael <princeloai70@gmail.com>
2024-11-22 18:53:21 +0200

    UPD nav bar at all

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+$(function () {
+    $(".my-workshop-list").slideUp();
+    if ($(window).width() < 445) {
+        $(".nav-links").slideUp();
+    }
+    $("#my-workshop").click(function () {
+        $(".my-workshop-list").css("visibility", "visible");
+        $(".my-workshop-list").slideToggle("fast");
+    });
+    $(".hamburger").click(function () {
+        $(".nav-links").css("visibility", "visible");
+        $(".nav-links").slideToggle("fast");
+    });
+})
