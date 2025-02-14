2e01d0b367dd9d112d61f68e230b5686d79ec682
Loai Wael <princeloai70@gmail.com>
2025-02-14 19:43:43 +0200

    UPD responsivnes && startup animations

--- JS/main.js
+++ JS/main.js
@@ -13,6 +13,7 @@ $(function () {
         $(".nav-links").slideToggle("fast");
     });
     contactAnimation();
+    workshopNavigationsAnimation()
     $("#certificate-button").click(function () {
         $("#certificates").slideDown("slow");
         document.getElementById("certificates").scrollIntoView();
@@ -28,6 +29,9 @@ $(function () {
         // $("#certificates-header").removeClass("certificates-animation-open");
         // $(".certificates-container").removeClass("certificates-animation-open");
     });
+    workshop_openSource_hover();
+    workshop_uiux_hover();
+    workshop_3d_hover();
 })
 
 function contactAnimation() {
@@ -39,4 +43,72 @@ function contactAnimation() {
         contactLinks[i].style.animationDuration = `${timing}ms`
         timing += 120
     }
+}
+function workshopNavigationsAnimation() {
+    const li = document.querySelectorAll(".navigations li");
+    var timing = 1500;
+    for (let i = 0; i < li.length; i++) {
+        li[i].style.animationName = 'navigations-li'
+        li[i].style.animationTimingFunction = 'cubic-bezier(1, 0, 0, 1)'
+        li[i].style.animationDuration = `${timing}ms`
+        timing += 150
+    }
+}
+
+function workshop_openSource_hover() {
+    const openSourceLi = document.querySelector("#open-source");
+    const websitesLi = document.querySelector("#websites");
+
+    openSourceLi.addEventListener("mouseenter", () => {
+        websitesLi.style.top = "calc(var(--top) - 2.5%)";
+        websitesLi.style.right = "calc((var(--top) / 16 + var(--right)) + 2.5%)";
+    });
+
+    openSourceLi.addEventListener("mouseleave", () => {
+        websitesLi.style.top = "";
+        websitesLi.style.right = "";
+    });
+}
+function workshop_uiux_hover() {
+    const uiux = document.querySelector("#ui-ux");
+    const openSourceLi = document.querySelector("#open-source");
+    const websitesLi = document.querySelector("#websites");
+
+    uiux.addEventListener("mouseenter", () => {
+        openSourceLi.style.top = "calc(var(--top) - 2.5%)";
+        openSourceLi.style.right = "calc((var(--top) / 16 + var(--right)) - 1%)";
+        websitesLi.style.top = "calc(var(--top) - 2.5%)";
+        websitesLi.style.right = "calc((var(--top) / 16 + var(--right)) + 2.5%)";
+    });
+
+    uiux.addEventListener("mouseleave", () => {
+        openSourceLi.style.top = "";
+        openSourceLi.style.right = "";
+        websitesLi.style.top = "";
+        websitesLi.style.right = "";
+    });
+}
+function workshop_3d_hover() {
+    const modeling = document.querySelector("#modeling");
+    const uiux = document.querySelector("#ui-ux");
+    const openSourceLi = document.querySelector("#open-source");
+    const websitesLi = document.querySelector("#websites");
+
+    modeling.addEventListener("mouseenter", () => {
+        uiux.style.top = "calc(var(--top) - 2.5%)";
+        uiux.style.right = "calc((var(--top) / 16 + var(--right)) - 4%)";
+        openSourceLi.style.top = "calc(var(--top) - 2.5%)";
+        openSourceLi.style.right = "calc((var(--top) / 16 + var(--right)) - 1%)";
+        websitesLi.style.top = "calc(var(--top) - 2.5%)";
+        websitesLi.style.right = "calc((var(--top) / 16 + var(--right)) + 2.5%)";
+    });
+
+    modeling.addEventListener("mouseleave", () => {
+        uiux.style.top = "";
+        uiux.style.right = "";
+        openSourceLi.style.top = "";
+        openSourceLi.style.right = "";
+        websitesLi.style.top = "";
+        websitesLi.style.right = "";
+    });
 }
